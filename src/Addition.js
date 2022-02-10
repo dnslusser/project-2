22379ca9670c9dfb66236fc944e8b088ae6f88fc
dnslusser <dslusser@madisoncollege.edu>
2022-02-10 12:29:52 -0600

    Math for addition component

--- src/Addition.js
+++ src/Addition.js
@@ -4,11 +4,8 @@ const Addition = (props) => {
   return (
     <>
       <h2>
-        Hello! {props.numberOne}
-      </h2>
-      <h3>
         {props.numberOne} + {props.numberTwo} = {props.numberOne + props.numberTwo}
-      </h3>
+      </h2>
     </>
   );
 };
